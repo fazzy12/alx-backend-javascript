a54de9875b135bf6cc0a50a4fc72b6314af7a1ac
fazzy12 <kaluifeanyi00@gmail.com>
2023-05-17 15:41:38 -0700

    a program named 1-stdin.js that will be executed through command line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,30 @@
-process.stdin.setEncoding('utf8');
+// process.stdin.setEncoding('utf8');
+
+// process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// process.stdin.on('data', function (data) {
+//     const name = data.toString().trim();
+//     if (name) {
+//         process.stdout.write(`Your name is: ${name}\n`);
+//         process.exit();
+//     }
+// });
+
+// process.on('exit', function () {
+//     process.stdout.write('This important software is now closing\n');
+// });
+
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', function (data) {
-    const name = data.toString().trim();
-    if (name) {
-        process.stdout.write(`Your name is: ${name}\n`);
-        process.exit();
+process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+
+    if (chunk) {
+        process.stdout.write(`Your name is: ${chunk}`);
     }
 });
 
-process.on('exit', function () {
+process.stdin.on('end', () => {
     process.stdout.write('This important software is now closing\n');
 });
