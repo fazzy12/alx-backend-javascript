6aa2e021bc63d77eeb12bac07a2e8209408abcfc
fazzy12 <kaluifeanyi00@gmail.com>
2024-01-15 16:11:18 +0100

    feat: Implement createIteratorObject function

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,34 +1,9 @@
 export default function createIteratorObject (report) {
-  const allEmployees = report.allEmployees;
-  const departments = Object.keys(allEmployees);
-  let currentIndex = 0;
-  let currentDepartmentIndex = 0;
-
-  const iterator = {
-    next() {
-      if (currentDepartmentIndex >= departments.length) {
-        return { done: true };
+  return (function * _ () {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
-
-      const currentDepartment = departments[currentDepartmentIndex];
-      const employeesInDepartment = allEmployees[currentDepartment];
-
-      if (currentIndex >= employeesInDepartment.length) {
-        currentIndex = 0;
-        currentDepartmentIndex += 1;
-        return this.next();
-      }
-
-      const result = {
-        value: employeesInDepartment[currentIndex],
-        done: false
-      };
-
-      currentIndex += 1;
-
-      return result;
     }
-  };
-
-  return iterator;
+  }());
 }
