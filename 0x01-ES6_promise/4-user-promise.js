bee8c9b32e0aa10aea92ecc4ba20a75efd2f81e6
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-11 01:07:58 +0100

    Implement signUpUser function to return a resolved Promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,5 @@
 // 4-user-promise.js
 
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName,
-    lastName
-  });
+  return Promise.resolve({ firstName, lastName });
 }
