86766b3e8c52829044773883c7654fa89f2c6232
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-12 09:06:22 +0100

    Implement guardrail function to handle return values and errors from mathFunction

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,16 +1,16 @@
 // 9-try.js
 
-export default function guardrail(mathFunction) {
-    const queue = [];
+export default function guardrail (mathFunction) {
+  const queue = [];
 
-    try {
-        const result = mathFunction();
-        queue.push(result);
-    } catch (error) {
-        queue.push(error);
-    } finally {
-        queue.push('Guardrail was processed');
-    }
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
 
-    return queue;
+  return queue;
 }
