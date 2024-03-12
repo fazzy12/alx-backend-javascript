10f4680b3f63036db6965168cc6bd27b3a35dad9
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-12 09:38:52 +0100

    Implement asyncUploadUser function to handle asynchronous calls to uploadPhoto and createUser

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser() {
-  try{
-      const photo = await uploadPhoto();
-      const user = await createUser();
-      return { photo, user };
+export default async function asyncUploadUser () {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
   }
-    catch (error){
-        return { photo: null, user: null };
-    }
 }
