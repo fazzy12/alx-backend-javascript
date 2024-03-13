202f1df04fba3eeeacac71806088be4377babb03
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-13 04:00:19 +0100

    Implement Pricing class with amount, currency

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,32 +1,44 @@
-import Currency from './3-currency';
+import Currency from './3-currency.js';
 
 export default class Pricing {
-  constructor(amount, Currency) {
+  constructor(amount, currency) {
     this._amount = amount;
-    this._Currency = Currency;
+    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(val) {
-    this._amount = val;
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
   }
 
-  get Currency() {
-    return this._Currency;
+  get currency() {
+    return this._currency;
   }
 
-  set Currency(val) {
-    this._Currency = val;
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    } else {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
   }
 
   displayFullPrice() {
-    return `${this._Currency.displayFullCurrency()} ${this._amount}`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    } else {
+      throw new TypeError('Both amount and conversion rate must be numbers');
+    }
   }
 }
