dbda3710ae26d51e60e8d6e73ddff17c5d00e7d9
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-13 03:08:24 +0100

    Fix type validation error in HolbertonCourse class for the 'name' attribute.

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,41 +1,39 @@
 export default class HolbertonCourse {
-  constructor(name, length, students) {
-    if (typeof name !== 'string') throw TypeError('name must be a string');
-    if (typeof length !== 'number') throw TypeError('length must be a number');
-    if (students.constructor !== Array && students.every((el) => typeof el === 'string')) {
-      throw TypeError('students must be an array of strings');
-    }
+  constructor (name, length, students) {
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  get name() {
+  get name () {
     return this._name;
   }
 
-  get length() {
+  set name (newName) {
+    if (typeof newName === 'string' || newName instanceof String) {
+      this._name = newName.toString();
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length () {
     return this._length;
   }
 
-  get students() {
+  set length (newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students () {
     return this._students;
   }
 
-  set name(newName) {
-    if (typeof newName !== 'string') throw TypeError('name must be a string');
-    this._name = newName;
-  }
-
-  set length(newLength) {
-    if (typeof newLength !== 'number') throw TypeError('length must be a number');
-    this._length = newLength;
-  }
-
-  set students(newStudents) {
-    if (newStudents.constructor !== Array && newStudents.every((el) => typeof el === 'string')) {
-      throw TypeError('students must be an array of strings');
-    }
+  set students (newStudents) {
     this._students = newStudents;
   }
 }
