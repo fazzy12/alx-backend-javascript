046e395a12a351154433399a74eb91a7b182ed1c
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-13 03:33:05 +0100

    Implement Currency class with constructor attributes 'code' and 'name'

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,26 +1,26 @@
-export default class Currency {
-  constructor(code, name) {
-    this._code = code;
-    this._name = name;
-  }
+export default class Currency{
+    constructor(code, name){
+        this._code = code;
+        this._name = name;
+    }
 
-  get code() {
-    return this._code;
-  }
+    get code(){
+        return this._code;
+    }
 
-  get name() {
-    return this._name;
-  }
+    set code(val){
+        this._code = val;
+    }
 
-  set code(newCode) {
-    this._code = newCode;
-  }
+    get name(){
+        return this._name;
+    }
 
-  set name(newName) {
-    this._name = newName;
-  }
+    set name(val){
+        this._name = val;
+    }
 
-  displayFullCurrency() {
-    return `${this._name} (${this._code})`;
-  }
+    displayFullCurrency(){
+        return `${this._name} (${this._code})`;
+    }
 }
