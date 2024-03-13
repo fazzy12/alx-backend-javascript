7509ad84965e2e4812dfacc0a541c1a3b3f6b621
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-13 01:48:49 +0100

    Implement initializeRooms function to create and return an array of ClassRoom objects with specific sizes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
-import ClassRoom from './0-classroom'
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms(){
-    return [new ClassRoom(10), new ClassRoom(20), new ClassRoom(30)];
+export default function initializeRooms() {
+  return [new ClassRoom(10), new ClassRoom(20), new ClassRoom(30)];
 }
