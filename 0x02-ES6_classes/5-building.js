e504918e45025be583b4ea334d206fe921e49779
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-18 21:52:23 +0100

    Implemented a class named Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,14 @@
+// 5-building.js
 export default class Building {
-  constructor(sqft) {
-    if (
-      this.constructor !== Building
-        && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw Error(
-        'Class extending Building must override evacuationWarningMessage',
-      );
-    }
-
+  constructor (sqft) {
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
