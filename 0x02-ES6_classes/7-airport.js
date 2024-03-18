37e6eec5049fc4fb9c79f3a72a7fca42ad2b2211
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-19 00:42:12 +0100

    Implemented the Airport class with constructor attributes name and code

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,26 +1,26 @@
-export default class Airport{
-    constructor(name, code){
-        this._name = name;
-        this._code = code;
-    }
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-    get name(){
-        return this._name
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(value){
-        this._name = value;
-    }
+  set name(value) {
+    this._name = value;
+  }
 
-    get code(){
-        return this._code
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(value){
-        this._code = value;
-    }
+  set code(value) {
+    this._code = value;
+  }
 
-    toString() {
-        return `[object ${this._code}]`;
-    }
+  toString() {
+    return `[object ${this._code}]`;
+  }
 }
