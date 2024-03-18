8f803e1f4f3f729cefc41d91e9c92b17ef3066ab
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-19 00:16:54 +0100

    Implemented the SkyHighBuilding class which extends from Building.

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,20 +1,20 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor (sqft, floors) {
+  constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
 
-  get sqft () {
+  get sqft() {
     return this._sqft;
   }
 
-  get floors () {
+  get floors() {
     return this._floors;
   }
 
-  evacuationWarningMessage () {
+  evacuationWarningMessage() {
     return `Evacuate slowly the ${this.floors} floors`;
   }
 }
