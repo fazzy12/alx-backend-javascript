00bc9dfd0ced79ee090d66ea265966e9265c39cb
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-19 13:10:06 +0100

    Implement getListStudentIds function using map method

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,3 @@
 export default function getListStudentIds(array) {
-  if (!Array.isArray(array)) return [];
-  return array.map((i) => i.id);
+  return Array.isArray(array) ? array.map(student => student.id) : [];
 }
