3102df5478a486ba0385b52c10d53ec51fa55deb
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-19 16:03:14 +0100

    Implement getStudentIdsSum function using reduce method

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-export default function getListStudentIds(array) {
-  return array.reduce((accumulator, i) => accumulator + i.id, 0);
+export default function getStudentIdsSum(getListStudents) {
+  return getListStudents.reduce((accum, curr) => accum + curr.id, 0);
 }
