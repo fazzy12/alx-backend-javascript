95e3e53fb9d30722de2db1b82104d4a43af3e0c0
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-19 02:05:02 +0100

    Implemented the Car class with a cloneCar method to create a new instance of the class

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,17 +1,12 @@
-export default class Car{
-    constructor(brand, motor, color){
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    cloneCar() {
-        const clonedCar = new this.constructor();
-        // Object.getOwnPropertyNames(this).forEach(prop => {
-        //     if (prop !== '_cloneCar') {
-        //         clonedCar[prop] = this[prop];
-        //     }
-        // });
-        return clonedCar;
-    }
+  cloneCar() {
+    const clonedCar = new this.constructor();
+    return clonedCar;
+  }
 }
