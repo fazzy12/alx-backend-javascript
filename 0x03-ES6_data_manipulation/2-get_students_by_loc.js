332e7969515d84c2c9d04410590913a23a072a53
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-19 15:40:58 +0100

    Fix getStudentsByLocation function to correctly filter students by city

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,6 @@
-export default function getStudentsByLocation(getListStudents, city) {
-  return getListStudents.filter(function(students) {
-    return (students.location === city);
-  })
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
 }
