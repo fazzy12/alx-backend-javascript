5a7f1a089b0db4fc25d7556f03b352192a02a665
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-20 10:56:17 +0100

    returns a string containing all the values from the set that start with the specified startString

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,16 @@
 export default function cleanSet(set, startString) {
+  const parts = [];
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  const valuesStartingWith = [...set].filter(value => value.startsWith(startString));
-  const cleanedValues = valuesStartingWith.map(value => value.slice(startString.length)).join('-');
-  return cleanedValues;
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
