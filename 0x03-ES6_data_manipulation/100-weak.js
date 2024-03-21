8acd252617f9c562f4ebe9c5fc941013a0782531
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-21 16:39:35 +0100

    feat: Implement weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,16 @@
-export const weakMap = new WeakMap();
+const weakMap = new WeakMap();
 
-export function queryAPI(endpoint) {
-  if (weakMap.has(endpoint)) {
-    const nQuery = weakMap.get(endpoint);
-    if (nQuery >= 4) throw Error('Endpoint load is high');
-    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  } else { weakMap.set(endpoint, 1); }
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    let count = weakMap.get(endpoint);
+    count++;
+    weakMap.set(endpoint, count);
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
 }
+
+export { weakMap, queryAPI };
