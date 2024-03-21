8b01f63fc8d39c39819c7745c86cf066cbd590df
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-21 15:09:48 +0100

    returns a map of groceries with the following items (name, quantity.:

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,6 +1,10 @@
 export default function groceriesList() {
-  const arr = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
-  const grocery = new Map();
-  for (const i of arr) grocery.set(i[0], i[1]);
-  return grocery;
+  const groceries = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ];
+  return new Map(groceries);
 }
