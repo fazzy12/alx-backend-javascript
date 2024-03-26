09263e4f2ce4be3f8c0d4fca6a6bb92f96d2d6e4
fazzy12 <kaluifeanyi00@gmail.com>
2024-03-26 01:43:33 +0100

    feat(task_0): Implement interface for Student and render table with student data

--- 0x04-TypeScript/task_0/js/main.js
+++ 0x04-TypeScript/task_0/js/main.js
@@ -1,29 +1,28 @@
-var studentOne = {
-    firstName: 'Syd',
-    lastName: 'Barrett',
-    age: 60,
-    location: 'Cambridge'
+// two students are created
+var student1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    location: 'USA'
 };
-var studentTwo = {
-    firstName: 'Will',
-    lastName: 'Oldham',
-    age: 51,
-    location: 'Louisville'
+var student2 = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 24,
+    location: 'UK'
 };
-var studentsList = [studentOne, studentTwo];
-var table = document.createElement('table');
-document.body.appendChild(table);
-var head = table.createTHead();
-var row = head.insertRow();
-var th1 = row.insertCell(0);
-var th2 = row.insertCell(1);
-th1.innerHTML = ("<b>First Name</b>");
-th2.innerHTML = ("<b>Location</b>");
-var body = table.createTBody();
-studentsList.map(function (student) {
-    var newRow = body.insertRow();
-    var firstNameRow = newRow.insertCell();
-    var locationRow = newRow.insertCell();
-    firstNameRow.innerHTML = student.firstName;
-    locationRow.innerHTML = student.location;
+// array of students
+var studentsList = [student1, student2];
+function render() {
+    var table = document.querySelector("#table");
+    studentsList.forEach(function (student) {
+        var row = table.insertRow();
+        var cellFirstName = row.insertCell();
+        var cellLocation = row.insertCell();
+        cellFirstName.textContent = student.firstName;
+        cellLocation.textContent = student.location;
+    });
+}
+document.addEventListener("DOMContentLoaded", function () {
+    render();
 });
--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,39 +1,44 @@
 interface Student {
-	firstName: string;
-	lastName: string;
-	age: number;
-	location: string;
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
 }
 
-const studentOne: Student = {
-	firstName: 'Syd',
-	lastName: 'Barrett',
-	age: 60,
-	location: 'Cambridge'
+// two students are created
+const student1: Student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    location: 'USA'
+};
+
+const student2: Student = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 24,
+    location: 'UK'
+};
+
+// array of students
+const studentsList: Student[] = [student1, student2];
+
+function render(): void {
+
+    const table: HTMLTableElement | null = document.querySelector("#table");
+
+studentsList.forEach((student: Student) => {
+    const row: HTMLTableRowElement = table.insertRow();
+
+    const cellFirstName: HTMLTableCellElement = row.insertCell();
+    const cellLocation: HTMLTableCellElement = row.insertCell();
+
+    cellFirstName.textContent = student.firstName;
+    cellLocation.textContent = student.location;
+});
+
 }
 
-const studentTwo: Student = {
-	firstName: 'Will',
-	lastName: 'Oldham',
-	age: 51,
-	location: 'Louisville'
-}
-
-const studentsList = [studentOne, studentTwo];
-const table: HTMLTableElement = document.createElement('table');
-
-document.body.appendChild(table);
-const head: HTMLTableSectionElement = table.createTHead();
-const row: HTMLTableRowElement = head.insertRow();
-const th1: HTMLTableHeaderCellElement = row.insertCell(0);
-const th2: HTMLTableHeaderCellElement = row.insertCell(1);
-th1.innerHTML = ("<b>First Name</b>");
-th2.innerHTML = ("<b>Location</b>");
-const body: HTMLTableSectionElement = table.createTBody();
-studentsList.map((student) => {
-	const newRow: HTMLTableRowElement = body.insertRow();
-	const firstNameRow: HTMLTableCellElement = newRow.insertCell();
-	const locationRow: HTMLTableCellElement= newRow.insertCell();
-	firstNameRow.innerHTML = student.firstName;
-	locationRow.innerHTML = student.location;
-})
+document.addEventListener("DOMContentLoaded", () => {
+    render();
+});
